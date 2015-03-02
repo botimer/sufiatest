3a4000c8079cecff5b726cf05787001e2c8fb088
Noah Botimer <botimer@umich.edu>
2015-03-02 13:33:55 -0500

    Iniital commit

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '359bd7ac6e29c413d8cce35a82a52b621877beb2d3fa639b9dab306792e97c02d2c8a0981c42db06173c252dbc1f871fa9a67e7c986affc67be73c335949dcff'
+
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  blacklight_for :catalog
+  devise_for :users
+  Hydra::BatchEdit.add_routes(self)
+  # This must be the very last route in the file because it has a catch-all route for 404 errors.
+    # This behavior seems to show up only in production mode.
+    mount Sufia::Engine => '/'
+  root to: 'homepage#index'
+end
